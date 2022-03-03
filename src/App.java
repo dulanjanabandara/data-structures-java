194bcc6b2b38e112fd3bbcd088e02cb8c4790a0a
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-03-03 22:44:50 +0530

    LinkedList: addLast() method for adding a new node at the end of the linked list was implemented.

--- src/App.java
+++ src/App.java
@@ -1,20 +1,26 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        // 1) Array
-        Array numbers = new Array(3);
-        numbers.insert(10);
-        numbers.insert(20);
-        numbers.insert(30);
-        numbers.insert(25);
-        numbers.insert(75);
-        numbers.insert(26);
-        numbers.insert(49);
-        numbers.insert(4);
-        // System.out.println(numbers.indexOf(100));
-        // numbers.removeAt(4);
-        numbers.print();
-        System.out.println(numbers.max());
+        // 2) Linked List
+        var list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+
+        // // 1) Array
+        // Array numbers = new Array(3);
+        // numbers.insert(10);
+        // numbers.insert(20);
+        // numbers.insert(30);
+        // numbers.insert(25);
+        // numbers.insert(75);
+        // numbers.insert(26);
+        // numbers.insert(49);
+        // numbers.insert(4);
+        // // System.out.println(numbers.indexOf(100));
+        // // numbers.removeAt(4);
+        // numbers.print();
+        // System.out.println(numbers.max());
     }
 }
 
