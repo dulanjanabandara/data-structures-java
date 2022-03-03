a24d940640eef4ed2fdb28422d15b25f33a20888
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-03-03 20:19:35 +0530

    insert() method was implemented. Dynamic array functionality was also implemented inside this insert() method.

--- src/App.java
+++ src/App.java
@@ -1,6 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         Array numbers = new Array(3);
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(40);
         numbers.print();
     }
 }
--- src/Array.java
+++ src/Array.java
@@ -7,10 +7,28 @@ public class Array {
         items = new int[length];
     }
 
+    public void insert(int item) {
+        // If the array is full, resize it
+        if (items.length == count) {
+            // Create a new array (twice the size of the array)
+            int[] newItems = new int[count * 2];
+
+            // Copy all the existing items
+            for (int i = 0; i < count; i++) {
+                newItems[i] = items[i];
+            }
+
+            // Set "items" to this new array
+            items = newItems;
+        }
+        
+        // Add the new item at the end
+        items[count++] = item;
+    }
+
     public void print() {
         for(int i = 0; i < count; i++) {
             System.out.println(items[i]);
         }
     }
-
 }
